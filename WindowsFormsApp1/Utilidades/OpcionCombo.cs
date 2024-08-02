5be3f2314c3e609a94d3932778a2415d898cdf84
MarinaProg1 <153437784+MarinaProg1@users.noreply.github.com>
2024-08-02 20:14:30 -0300

    Agreue cartepa opcion combo

---
+++ WindowsFormsApp1/Utilidades/OpcionCombo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Utilidades
+{
+    public class OpcionCombo
+    {
+        public String Texto {  get; set; }  
+        public object Valor { get; set; }   
+    }
+}
